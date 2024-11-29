ecf13cc60f0706fab7d0cbb76910c49e59177aeb
Bohdan Petrenko <125706158+NotFAIRIZ@users.noreply.github.com>
2024-11-29 16:34:33 +0200

    Delete Task2 directory

--- Task2/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        UsaPictureBuilder p1 = new UsaPictureBuilder();
-        p1.setBasicParams("name",31, "aaaaaa").build();
-        p1.toString();
-
-    }
-}
